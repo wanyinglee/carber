aaa92c64d6d0143a672e582dfc4d8a254e9a1697
Joyce Lee <joycelee0411@gmail.com>
2021-04-26 22:25:49 +0800

    change table to ag-grid. update table data. able to sort and filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,137 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
-import { Table } from 'antd';
-import 'antd/dist/antd.css';
+import { AgGridReact } from 'ag-grid-react';
 
-const bybitSymbolMapping = {
-  BTCUSD: 'PERPETUAL',
-  BTCUSDM21: 'CURRENT_QUARTER',
-  BTCUSDU21: 'NEXT_QUARTER',
-};
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import './App.css';
 
-const columns = [
-  {
-    title: 'Symbol',
-    dataIndex: 'symbol',
-    key: 'symbol',
-  },
-  {
-    title: 'Price',
-    dataIndex: 'price',
-    key: 'price',
-    render: (text, record, index) => Number(text).toFixed(1),
-  },
-  {
-    title: 'Spot Price',
-    dataIndex: 'spotPrice',
-    key: 'spotPrice',
-    render: (text, record, index) => Number(text).toFixed(1),
-  },
-  {
-    title: 'Basis',
-    dataIndex: 'basis',
-    key: 'basis',
-    render: (text, record, index) => `${(text * 100).toFixed(1)}%`,
-  },
-];
+const gridOptions = {
+  columnsDefs: [
+    {
+      headerName: 'Symbol',
+      field: 'symbol',
+      filter: 'agTextColumnFilter',
+    },
+    {
+      headerName: 'Pair',
+      field: 'pair',
+      sort: 'asc',
+      filter: 'agTextColumnFilter',
+      filterParams: {
+        filterOptions: ['contains', 'startsWith'],
+        defaultOption: 'startsWith',
+        newRowsAction: 'keep',
+      },
+    },
+    {
+      headerName: 'Price',
+      field: 'price',
+    },
+    {
+      headerName: 'Spot Price',
+      field: 'spotPrice',
+    },
+    {
+      headerName: 'Basis',
+      field: 'basis',
+      valueFormatter: param => `${(Number(param.value) * 100).toFixed(4)}%`,
+    },
+    {
+      headerName: 'Annualized Basis',
+      field: 'annualizedBasis',
+      valueFormatter: param => `${(Number(param.value) * 100).toFixed(1)}%`,
+    }
+  ],
+  defaultColGroupDef: {
+    editable: false,
+    resizable: true,
+    sortable: true,
+  }
+}
 
 const App = () => {
-  const [data, setData] = useState([]);
-  const [bybitData, setBybitData] = useState([]);
-  // Binance
-  const getPerpetualBasis = () => {
-    return axios.get('https://dapi.binance.com/futures/data/basis?pair=BTCUSD&contractType=PERPETUAL&period=5m&limit=1');
-  };
-  const getCurrentQuarterBasis = () => {
-    return axios.get('https://dapi.binance.com/futures/data/basis?pair=BTCUSD&contractType=CURRENT_QUARTER&period=5m&limit=1');
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+
+  const [exchangeInfo, setExchangeInfo] = useState([]);
+  const [rowData, setRowData] = useState([]);
+
+  const getPremiumIndex = () => {
+    return axios.get('https://dapi.binance.com/dapi/v1/premiumIndex');
   }
-  const getNextQuarterBasis = () => {
-    return axios.get('https://dapi.binance.com/futures/data/basis?pair=BTCUSD&contractType=NEXT_QUARTER&period=5m&limit=1');
+  const getExchangeInfo = () => {
+    return axios.get('https://dapi.binance.com/dapi/v1/exchangeInfo');
   }
-  // Bybit
-  const getByBitBasis = () => {
-    return axios.get('https://api.bybit.com/v2/public/tickers', {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Accept': '*/*',
-        'Access-Control-Allow-Methods': 'PUT, POST, GET, DELETE, PATCH, OPTIONS',
-      }
-    });
-  }
-  const getByBitRate = (record) => {
-    switch (record.symbol) {
-      case 'BTCUSD':
-        return record.predicted_funding_rate;
-      case 'BTCUSDM21':
-      case 'BTCUSDU21':
-        return record.mark_price / record.index_price - 1;
+
+  const getBasisRate = (indexInfo) => {
+    if (indexInfo.symbol.indexOf('_PERP') > -1) {
+      return Number(indexInfo.lastFundingRate);
+    } else {
+      return Number(indexInfo.markPrice) / Number(indexInfo.indexPrice) - 1;
     }
-  }
+  };
 
-  const getBasisRate = (type, rate) => {
-    const now = new Date();
-    const curExpiryDate = new Date(2021, 5, 25, 16);
-    const curRatio = (365 * 24 * 3600 / ((curExpiryDate - now) / 1000));
-    const nextExpiryDate = new Date(2021, 8, 24, 16);
-    const nextRatio = (365 * 24 * 3600 / ((nextExpiryDate - now) / 1000));
-
-    switch (type) {
-      case 'PERPETUAL':
-        return Math.pow(1 + Number(rate), 365 * 3) - 1;
-      case 'CURRENT_QUARTER':
-        return Math.pow(1 + Number(rate), curRatio) - 1;
-      case 'NEXT_QUARTER':
-        return Math.pow(1 + Number(rate), nextRatio) - 1;
-      default:
-        return 0;
+  const getAnnualizedBasis = (indexInfo, exchangeInfo) => {
+    const rate = getBasisRate(indexInfo);
+    if (indexInfo.symbol.indexOf('_PERP') > -1) {
+      return Math.pow(1 + Number(indexInfo.lastFundingRate), 365 * 3) - 1;
+    } else {
+      const expiryDate = exchangeInfo.filter(info => info.symbol === indexInfo.symbol)[0].deliveryDate;
+      const currentDate = indexInfo.time;
+      const ratio = 365 * 24 * 3600 * 1000 / (expiryDate - currentDate);
+      return Math.pow(1 + Number(rate), ratio) - 1;
     }
   }
 
   useEffect(() => {
-    Promise.all([getPerpetualBasis(), getCurrentQuarterBasis(), getNextQuarterBasis()]).then(response => {
-      const result = [...response[0].data, ...response[1].data, ...response[2].data];
-      setData(result.map(r => ({ symbol: r.contractType, price: r.futuresPrice, spotPrice: r.indexPrice, basis: getBasisRate(r.contractType, r.basisRate)})));
+    getExchangeInfo().then(response => {
+      const result = response.data.symbols;
+      setExchangeInfo(result);
     });
-    // getByBitBasis().then(response => {
-    //   const btcResult = response.result.filter(r => r.symbol.startWith('BTCUSD'));
-    //   const bybitData = btcResult.map(r => ({ symbol: bybitSymbolMapping(r.symbol), price: r.mark_price, spotPrice: r.index_price, basis: getBasisRate(bybitSymbolMapping(r.symbol), getByBitRate(r)) }));
-    //   setBybitData(bybitData);
-    // });
 
     const interval = setInterval(() => {
-      Promise.all([getPerpetualBasis(), getCurrentQuarterBasis(), getNextQuarterBasis()]).then(response => {
-        const result = [...response[0].data, ...response[1].data, ...response[2].data];
-        setData(result.map(r => ({ symbol: r.contractType, price: r.futuresPrice, spotPrice: r.indexPrice, basis: getBasisRate(r.contractType, r.basisRate) })));
+      getExchangeInfo().then(response => {
+        const result = response.data.symbols;
+        setExchangeInfo(result);
       });
-      // getByBitBasis().then(response => {
-      //   const btcResult = response.result.filter(r => r.symbol.startWith('BTCUSD'));
-      //   const bybitData = btcResult.map(r => ({ symbol: bybitSymbolMapping(r.symbol), price: r.mark_price, spotPrice: r.index_price, basis: getBasisRate(bybitSymbolMapping(r.symbol), getByBitRate(r)) }));
-      //   setBybitData(bybitData);
-      // });
-    }, 30 * 1000);
+    }, 3600 * 1000);
+
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    if (exchangeInfo && exchangeInfo.length) {
+      getPremiumIndex().then(response => {
+        const basisData = response.data.map(r => ({ symbol: r.symbol, pair: r.pair, price: r.markPrice, spotPrice: r.indexPrice, basis: getBasisRate(r), annualizedBasis: getAnnualizedBasis(r, exchangeInfo) }));
+        setRowData(basisData);
+      });
+
+      const interval = setInterval(() => {
+        getPremiumIndex().then(response => {
+          const basisData = response.data.map(r => ({ symbol: r.symbol, pair: r.pair, price: r.markPrice, spotPrice: r.indexPrice, basis: getBasisRate(r), annualizedBasis: getAnnualizedBasis(r, exchangeInfo) }));
+          setRowData(basisData);
+        });
+      }, 30 * 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [exchangeInfo]);
+
+  const onGridReady = params => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+  };
+
+  const onFirstDataRendered = params => {
+    gridApi.sizeColumnsToFit();
+  };
+
   return (
     <div className="App">
-      {data.length && <Table dataSource={data} columns={columns} />}
-      {/* {bybitData.length && <Table dataSource={bybitData} columns={columns} />} */}
+      <div className="ag-theme-alpine" style={{ height: '100vh' }}>
+        <AgGridReact onGridReady={onGridReady} rowData={rowData} columnDefs={gridOptions.columnsDefs} defaultColDef={gridOptions.defaultColGroupDef} onFirstDataRendered={onFirstDataRendered} />
+      </div>
     </div>
   );
 }
